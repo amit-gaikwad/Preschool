addc9e66fc4a8956f16a886e95d1f230173e7656
rahul_dudhane <rahul.y.dudhane@gmail.com>
2018-08-14 17:27:27 +0530

    Done code formatting

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,19 +1,16 @@
 import {Injectable} from "@angular/core";
 import { Http ,Response} from "@angular/http";
-
 import {  Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
 
 @Injectable()
 export class YoutubeService {
+    
     apiKey = "AIzaSyAcviOah4O8RZKGX_BgGQkALryXRqTl-JM";
     channelId = "UCeMSLh2vRcYgV_ahmgYWzRA";
     youtubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+this.channelId+"&order=date&type=video&key="+this.apiKey;
-    
-    youtubeData = [];
 
     constructor(private http:Http){
-    
     }
 
     getAll() : Observable<any> {
--- src/app/video-page-component/video-page-component.ts
+++ src/app/video-page-component/video-page-component.ts
@@ -6,18 +6,17 @@ import{YoutubeService} from "../services/youtube.service"
     templateUrl:"./video-page-component.html",
     styleUrls:["./video-page-component.css"]
 })
-
 export class MyVideoPageComponent {
     videos = [];
     videoUrl = "https://www.youtube.com/embed/";
 
     constructor(private youtubeService : YoutubeService){
         youtubeService.getAll().subscribe((data)=>{
-            this.videos = data["items"];
-            this.videos = this.videos.map((item)=>{
-                return item.id.videoId;
-            });
-        });
+                     this.videos = data["items"];
+                     this.videos = this.videos.map((item)=>{
+                    return item.id.videoId;
+                  });
+          });
     }
 
     getVideoUrl( videoId : string) : any {
