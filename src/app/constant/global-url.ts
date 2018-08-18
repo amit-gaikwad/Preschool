a738c60a8763f0e4ed7d8388fac444c988c03f2a
samrudhi-Gamhire <42338068+samrudhi-Gamhire@users.noreply.github.com>
2018-08-18 17:00:10 +0530

    added labels in constants folder

--- src/app/constant/global-url.ts
+++ src/app/constant/global-url.ts
@@ -1,7 +1,7 @@
 
 export class GlobalUrl{
      static url = "http://localhost:4200/assets/gallary.json" ;
-
+     static labels =["all","indoor","outdoor","gd","babies","monthlyactivty","specialevent","birhday","schoolreadlines"];
 }
 
 
