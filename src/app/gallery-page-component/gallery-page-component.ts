94c60684ca5a6c7ea90e5dec16bb22c513da863d
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2019-01-05 23:24:57 +0530

    show only public images on the preschool

--- src/app/gallery-page-component/gallery-page-component.ts
+++ src/app/gallery-page-component/gallery-page-component.ts
@@ -1,26 +1,26 @@
-    import { Component } from "@angular/core";
-    import {  GalleryService } from "../services/gallery-service";
+    import { Component } from '@angular/core';
+    import {  GalleryService } from '../services/gallery-service';
     @Component({
-        selector:"my-gallery-component",
-        templateUrl:"./gallery-page-component.html",
-        styleUrls:["./gallery-page-component.css","../../assets/css/lightbox.min.css"]
-        
+        selector: 'my-gallery-component',
+        templateUrl: './gallery-page-component.html',
+        styleUrls: ['./gallery-page-component.css', '../../assets/css/lightbox.min.css']
+
     })
 
     export class MyGalleryPageComponent  {
-        
-        gallery = []; 
-        labels = ["All","Indoor","Outdoor","Group Activity","Babies","Monthly Activity","Special Event","Birthday","School Readiness"];
-        selectedcategary = "all";
-        constructor(private galleryservice : GalleryService ){
-        galleryservice.getAll().subscribe((data)=> {
+
+        gallery = [];
+        labels = ['All', 'Indoor', 'Outdoor', 'Group Activity', 'Babies', 'Monthly Activity', 'Special Event', 'Birthday', 'School Readiness'];
+        selectedcategary = 'all';
+        constructor(private galleryservice: GalleryService ) {
+        galleryservice.getAll().subscribe((data) => {
             this.gallery = data;
-        }); 
+        });
         }
-        oncategarychange(categary : string){
+        oncategarychange(categary: string) {
             this.selectedcategary = categary ;
-            this.galleryservice.getByCategary(this.selectedcategary).subscribe(data=> {
+            this.galleryservice.getByCategary(this.selectedcategary).subscribe(data => {
                 this.gallery = data;
             });
         }
-    }       
+    }
--- src/app/services/gallery-service.ts
+++ src/app/services/gallery-service.ts
@@ -1,38 +1,37 @@
-import { Injectable } from "@angular/core";
-import {Http,Response} from "@angular/http";
-import { Observable} from "rxjs";
-import { map, filter } from "rxjs/operators";
-import { AppConstants } from "../constants/app.constants";
+import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { Observable} from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import { AppConstants } from '../constants/app.constants';
 @Injectable()
 
-export class GalleryService{
+export class GalleryService {
 
-constructor(private http:Http){}
- 
-galleryUrl=AppConstants.serverUrl+'gallery';
+constructor(private http: Http) {}
 
-getAll() : Observable <any> {
+galleryUrl = AppConstants.serverUrl + 'gallery';
+
+getAll(): Observable <any> {
 return this.http.get(this.galleryUrl).
-pipe(map((res:Response)=>{
+pipe(map((res: Response) => {
     return res.json();
 }));
 }
 
-getByCategary(categary : string) : Observable <any>{
-    if(categary == 'All'){
+getByCategary(categary: string): Observable <any> {
+    if (categary === 'All') {
         return this.getAll();
-        }
-    else{
+        } else {
    return this.http.get(this.galleryUrl).
-   pipe(map( (resp : Response)=> {
-    var newData = resp.json();
-    var newArray = newData.filter((item)=>{
-        return categary == item.category;
-    })
+   pipe(map( (resp: Response) => {
+    const newData = resp.json();
+    const newArray = newData.filter((item) => {
+        return categary.toLowerCase() === item.category.toLowerCase();
+    });
     return newArray;
     }));
 }
- 
+
 }
 
 }
