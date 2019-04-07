16874041fc7bbeb75980758795fb06173cf08730
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2019-04-07 23:49:22 +0530

    change url of backend

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -7,6 +7,6 @@ export class AppConstants{
     //static youtubeChannelId = "UCITKX8xLM9BvvnMoai9nu6w";//New
     static youtubeApi = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+AppConstants.youtubeChannelId+"&order=date&type=video&key="+AppConstants.googleApiKey;
    // static eventUrl = "http://localhost:4200/assets/events.json";
-    static serverUrl ="https://funnydayschildcare.com.au:5000/";
-    static adminUrl = "https://funnydayschildcare.com.au:5555/parentlogin";
+    static serverUrl ="http://3.17.161.1:8000/";
+    static adminUrl = "http://3.17.161.1:5555/parentlogin";
 }
