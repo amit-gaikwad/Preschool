442959a45e8b607b3c8771ac734169a841bb3f4c
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2019-04-13 12:14:00 +0530

    change youtube id

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,12 +1,12 @@
 export class AppConstants{
     static youtubeUrl = "https://www.youtube.com/embed/";//Old
     //static youtubeUrl = "https://google.com/+dipakbiranjefunnydayschildcare";//New
-    static googleApiKey = "AIzaSyAcviOah4O8RZKGX_BgGQkALryXRqTl-JM";//OLD
+    static googleApiKey = "AIzaSyBO9fjMcQN94Wu38Uo-pkqvF9VkfHtEmv0";//OLD
     //static googleApiKey = "AIzaSyAlmxZ2-6JkGN44HPgcdRp00fDt_gWW1zo";//New
-    static youtubeChannelId = "UCeMSLh2vRcYgV_ahmgYWzRA"; //Old
+    static youtubeChannelId = "UCT51ktKAoNTfXjOnsB-8ixQ"; //Old
     //static youtubeChannelId = "UCITKX8xLM9BvvnMoai9nu6w";//New
     static youtubeApi = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+AppConstants.youtubeChannelId+"&order=date&type=video&key="+AppConstants.googleApiKey;
    // static eventUrl = "http://localhost:4200/assets/events.json";
-    static serverUrl ="http://3.17.161.1:8000/";
-    static adminUrl = "http://3.17.161.1:5555/parentlogin";
+    static serverUrl ="http://www.angulartric.com:8000/";
+    static adminUrl = "http://www.angulartric.com:5555/parentlogin";
 }
